207e86ccaad07549c7ca7e1a58f701cbc2bcad73
catboyking <mcintyre.ch@gmail.com>
2020-10-03 14:38:01 -0400

    Added custom texture importer

---
+++ Assets/Editor/CustomTextureImporter.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class CustomTextureImporter : AssetPostprocessor
+{
+    void OnPreprocessTexture()
+    {
+        var importer = assetImporter as TextureImporter;
+        importer.filterMode = FilterMode.Point;
+        importer.textureCompression = TextureImporterCompression.Uncompressed;
+        importer.spritePixelsPerUnit = 16;
+    }
+}
+
