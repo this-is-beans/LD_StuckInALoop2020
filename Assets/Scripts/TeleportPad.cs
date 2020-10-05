d8d169713a33fe77d69d30a5765d0454016e9a2b
Blynxy Furyfur <blynxy@cathead.media>
2020-10-04 20:44:07 -0500

    fix teleport pad location

--- Assets/Scripts/TeleportPad.cs
+++ Assets/Scripts/TeleportPad.cs
@@ -6,9 +6,12 @@ namespace DefaultNamespace {
         public GameObject TargetLocation;
         public BoxCollider2D boxCollider2D;
         private Collider2D[] interactables;
+        public bool isHorizontal;
 
         private void OnTriggerEnter2D(Collider2D obj) {
-            obj.transform.parent.transform.position = TargetLocation.transform.position;
+                obj.transform.parent.transform.position = TargetLocation.transform.position +
+                                                          (obj.transform.position - gameObject.transform.position);
+
         }
     }
 }
