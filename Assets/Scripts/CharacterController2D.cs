0e1d935a302d97be191bf6b31da6450deb09d6b7
Blynxy Furyfur <blynxy@cathead.media>
2020-10-06 15:01:35 -0500

    fix teleport

--- Assets/Scripts/CharacterController2D.cs
+++ Assets/Scripts/CharacterController2D.cs
@@ -6,8 +6,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
-public class CharacterController2D : MonoBehaviour
-{
+public class CharacterController2D : MonoBehaviour {
     // blynxy just copied catboy, cause wtf is this unity stuff
     public SpriteRenderer characterSpriteRenderer;
     Rigidbody2D rbody;
@@ -58,30 +57,25 @@ public class CharacterController2D : MonoBehaviour
     //     Handles.color = Color.cyan;
     //     Handles.DrawWireDisc(gameObject.transform.position, gameObject.transform.forward, interactableAreaRadius);
     // }
-
-    void Start()
-    {
+    void Start() {
         //animator = gameObject.GetComponentInChildren<Animator>();
         rbody = gameObject.GetComponent<Rigidbody2D>();
 
         startPosition = gameObject.transform.position;
-       //isBouncing = false;
-       //isFrozen = false;
-       //enableBounce = true;
-       //speed = 5;
-       //bounceSpeed = (speed <= 0) ? 5 : speed / 2;
-       //bounceMaxHeight = .2f;
-       //bounceMinHeight = .05f;
+        //isBouncing = false;
+        //isFrozen = false;
+        //enableBounce = true;
+        //speed = 5;
+        //bounceSpeed = (speed <= 0) ? 5 : speed / 2;
+        //bounceMaxHeight = .2f;
+        //bounceMinHeight = .05f;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if (!isFrozen)
-        {
+    void Update() {
+        if (!isFrozen) {
             characterSpriteRenderer.sortingOrder = (Mathf.RoundToInt(transform.position.y * 16f) * -1);
-            if (heldItem != null)
-            {
+            if (heldItem != null) {
                 heldItem.itemSpriteRenderer.sortingOrder = characterSpriteRenderer.sortingOrder + 1;
             }
 
@@ -92,12 +86,9 @@ public class CharacterController2D : MonoBehaviour
                 LayerMask.GetMask("Interactable"));
 
             Interactable targetInteractable = null;
-            if (interactables.Length > 0)
-            {
-                foreach (Collider2D i in interactables)
-                {
-                    if (i.TryGetComponent(out Interactable interactable))
-                    {
+            if (interactables.Length > 0) {
+                foreach (Collider2D i in interactables) {
+                    if (i.TryGetComponent(out Interactable interactable)) {
                         targetInteractable = interactable;
 
                         OnInteractableProximity?.Invoke(targetInteractable);
@@ -106,8 +97,7 @@ public class CharacterController2D : MonoBehaviour
                     }
                 }
             }
-            else
-            {
+            else {
                 OnInteractableClear?.Invoke();
             }
 
@@ -116,55 +106,44 @@ public class CharacterController2D : MonoBehaviour
              */
             moveVec2 = new Vector2();
             // movement: left right up down
-            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-            {
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
                 moveVec2 += Vector2.left;
             }
 
-            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-            {
+            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
                 moveVec2 += Vector2.right;
             }
 
-            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
-            {
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) {
                 moveVec2 += Vector2.up;
             }
 
-            if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
-            {
+            if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) {
                 moveVec2 += Vector2.down;
             }
 
-            if (moveVec2.magnitude > 0)
-            {
+            if (moveVec2.magnitude > 0) {
                 animator.Play("Base Layer.Walk", 0);
             }
-            else
-            {
+            else {
                 animator.Play("Base Layer.Idle", 0);
             }
             /***
              * INTERACTION SECTION
              */
 
-            if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.KeypadEnter))
-            {
-                if (targetInteractable)
-                {
+            if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.KeypadEnter)) {
+                if (targetInteractable) {
                     Interact(targetInteractable);
                 }
-                else
-                {
+                else {
                     if (heldItem) DropItem();
                 }
             }
 
-            if (Input.GetKeyDown(KeyCode.Q) || Input.GetKeyDown(KeyCode.Alpha0))
-            {
+            if (Input.GetKeyDown(KeyCode.Q) || Input.GetKeyDown(KeyCode.Alpha0)) {
                 // do action maybe
-                if (heldItem)
-                {
+                if (heldItem) {
                     DropItem();
                 }
             }
@@ -218,39 +197,31 @@ public class CharacterController2D : MonoBehaviour
         }
     }
 
-    private void FixedUpdate()
-    {
-        if (isFrozen)
-        {
+    private void FixedUpdate() {
+        if (isFrozen) {
             rbody.velocity = Vector2.zero;
         }
-        else
-        {
+        else {
             rbody.MovePosition(rbody.position + moveVec2 * speed * Time.deltaTime);
         }
     }
 
-    void Interact(Interactable target)
-    {
+    void Interact(Interactable target) {
         // do some stupid thing with things that are connected
 
         Item returnedItem = target.Interact(heldItem);
 
-        if (!returnedItem)
-        {
+        if (!returnedItem) {
             RemoveItem();
             //item was taken, nothing returned
         }
-        else if (returnedItem == heldItem)
-        {
+        else if (returnedItem == heldItem) {
             //item was not taken
         }
-        else if (returnedItem != heldItem)
-        {
+        else if (returnedItem != heldItem) {
             //item was taken and new item given
 
-            if (heldItem)
-            {
+            if (heldItem) {
                 RemoveItem();
             }
 
@@ -258,22 +229,18 @@ public class CharacterController2D : MonoBehaviour
         }
     }
 
-    void RemoveItem()
-    {
+    void RemoveItem() {
         OnRemoveItem?.Invoke();
         heldItem = null;
     }
 
-    void DropItem()
-    {
+    void DropItem() {
         heldItem.Dropped();
         RemoveItem();
     }
 
-    void AddItem(Item item)
-    {
-        if (heldItem != null)
-        {
+    void AddItem(Item item) {
+        if (heldItem != null) {
             Debug.LogError("Trying to add item about hands are occupied! (call remove item first)");
         }
 
@@ -288,26 +255,23 @@ public class CharacterController2D : MonoBehaviour
         OnReceiveItem?.Invoke(heldItem);
     }
 
-    public void Freeze()
-    {
+    public void Freeze() {
         this.isFrozen = true;
     }
 
-    public void UnFreeze()
-    {
+    public void UnFreeze() {
         this.isFrozen = false;
     }
 
-    public void ResetState()
-    {
-        if (heldItem != null)
-        {
+    public void ResetState() {
+        if (heldItem != null) {
             Item clone = heldItem.Clone();
             heldItem.doNotReset = false;
             heldItem.ResetState();
             RemoveItem();
             AddItem(clone);
         }
+
         gameObject.transform.localPosition = startPosition;
     }
 }
